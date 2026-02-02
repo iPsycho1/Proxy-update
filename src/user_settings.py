3c2814a048c554a680d8c454fa7925ee5ffc79bc
iPsycho <125604888+iPsycho1@users.noreply.github.com>
2026-02-03 03:23:24 +0330

    Update user_settings.py

--- src/user_settings.py
+++ src/user_settings.py
@@ -11,6 +11,7 @@ SOURCE_URLS = [
     "https://raw.githubusercontent.com/imalrzai/NebuVPNExclaveVirtual/refs/heads/main/NebuVPN",
     "https://raw.githubusercontent.com/zieng2/wl/main/vless_lite.txt#vless_lite",
     "https://raw.githubusercontent.com/iPsycho1/Subscription/refs/heads/main/CF%20New",
+    "https://unlckmy.net/sub/u1153432s4043099p70/b0128f702507821423d47ec5ddce5e9d",
     # Add more URLs here if you want to include additional sources.
 ]
 
